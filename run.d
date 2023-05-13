c1fd724c33ebe271558dc9a2139cf499de2c407c
Eng-Mohamed-Hesham <muhammed195910@feng.bu.edu.eg>
2023-05-14 02:12:27 +0300

    Update

---
+++ run.d
@@ -0,0 +1,6 @@
+.\run.o: logic\Run.c
+.\run.o: logic\../headers/tm4c123gh6pm.h
+.\run.o: logic\../headers/Nokia5110.h
+.\run.o: logic\../headers/TExaS.h
+.\run.o: logic\../headers/gpio_driver.h
+.\run.o: logic\../headers/draw.h
