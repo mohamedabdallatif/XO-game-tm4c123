edbce1f04b019f21ba460acb514a8def68d14795
Eng-Mohamed-Hesham <muhammed195910@feng.bu.edu.eg>
2023-05-19 00:50:43 +0300

    Repaire something

--- Source/logic/Run.c
+++ Source/logic/Run.c
@@ -66,6 +66,7 @@ void drawGrid(void){
 			Nokia5110_OutChar(turn);
 
 }
+
 void result(){
 		Nokia5110_ClearBuffer();
 		Nokia5110_DisplayBuffer();
@@ -83,72 +84,70 @@ void result(){
 }
 
 void Xo_Init(void){
-			Nokia5110_Clear();
-	Nokia5110_SetCursor(4,1);
-	Nokia5110_OutString("1.Kit");
-	Nokia5110_SetCursor(4,2);
-	Nokia5110_OutString("2.UART");
+		Nokia5110_Clear();
+		Nokia5110_SetCursor(4,1);
+		Nokia5110_OutString("1.Kit");
+		Nokia5110_SetCursor(4,2);
+		Nokia5110_OutString("2.UART");
 }
 void Bye(void)
 {		
 	  Nokia5110_Clear();
 		Nokia5110_SetCursor(2,2);
 		Nokia5110_OutString("Bye Bye!");
-	while(1){
-	}
+		while(1){
+		}
 }
 void Play_Again(void)
 {
 		Nokia5110_Clear();
 		Xo_Init();
-  i = 0, j = 0, position = 1, current_cursor = 1;
-	moves = 0;
-	cursor=1;
-	for (i = 0; i < 9; i++)
-		matrix[i] = ' '; // erase all drawings in cells matrix
-	turn = 'X';
-	flag = 0;
+		i = 0, j = 0, position = 1, current_cursor = 1;
+		moves = 0;
+		cursor=1;
+		for (i = 0; i < 9; i++) matrix[i] = ' '; // erase all drawings in cells matrix
+		turn = 'X';
+		flag = 0;
 }
 void Print_Result(char turn){
-	
-	extern int flag ;
-	flag = 3;
-	Nokia5110_Clear();
-	Nokia5110_SetCursor(0,0);
-	if(turn == 'D')
-	{
-			Nokia5110_OutString("Draw!");
-	}
-	else if(turn == 'X')
-	{
-			Nokia5110_OutString("Player X Won");
-	}
-	else if(turn == 'O')
-	{
-			Nokia5110_OutString("Player O Won");
-	}
-	    Nokia5110_SetCursor(2,10);
-			Nokia5110_OutString("Score X!");
-	    Nokia5110_OutUDec(scoreX);
+		extern int flag ;
+		flag = 3;
+		Nokia5110_Clear();
+		Nokia5110_SetCursor(0,0);
+		if(turn == 'D')
+		{
+				Nokia5110_OutString("Draw!");
+		}
+		else if(turn == 'X')
+		{
+				Nokia5110_OutString("Player X Won");
+		}
+		else if(turn == 'O')
+		{
+				Nokia5110_OutString("Player O Won");
+		}
+				Nokia5110_SetCursor(2,10);
+				Nokia5110_OutString("Score X!");
+				Nokia5110_OutUDec(scoreX);
 
-			Nokia5110_SetCursor(50,10);
-			Nokia5110_OutString("Score Y!");
-	    Nokia5110_OutUDec(scoreO);
+				Nokia5110_SetCursor(50,10);
+				Nokia5110_OutString("Score Y!");
+				Nokia5110_OutUDec(scoreO);
+			
+				/*Timer2_Init(300);
+				Nokia5110_Clear();
 		
-			/*Timer2_Init(300);
-			Nokia5110_Clear();
-	
-			Nokia5110_SetCursor(2,2);
-			Nokia5110_OutString("Play Again?");
-	    
-			Nokia5110_SetCursor(50,2);
-			Nokia5110_OutString("YES!");
-	  
+				Nokia5110_SetCursor(2,2);
+				Nokia5110_OutString("Play Again?");
+				
+				Nokia5110_SetCursor(50,2);
+				Nokia5110_OutString("YES!");
+			
 
-			Nokia5110_SetCursor(50,10);
-			Nokia5110_OutString("NO!");
-	*/
-	}
+				Nokia5110_SetCursor(50,10);
+				Nokia5110_OutString("NO!");
+		*/
+		}
 
 
 
